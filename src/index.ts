71b93300e01392c8c5e10c15e368655026cbbb67
Peter <petecruckshank@gmail.com>
2022-03-08 15:07:32 -0500

    ✨init streakCounter( ) for main functionality

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { formattedDate } from '../src/lib';
+
+interface Streak  {
+  currentCount: number;
+  startDate: string;
+  lastLoginDate: string;
+}
+
+
+
+export function streakCounter(_localStorage: Storage, date: Date): Streak {
+  return {
+    currentCount: 1,
+    startDate: formattedDate(date),
+    lastLoginDate:formattedDate(date)
+  }
+}
