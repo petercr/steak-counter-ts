9b74837da33b7852d59ca26623f5177b501a62a3
Peter <petecruckshank@gmail.com>
2022-03-08 15:10:04 -0500

    ✅ added tests for dateForm..() & streakCounter()

--- __tests__/index.tests.ts
+++ __tests__/index.tests.ts
@@ -1,5 +1,38 @@
+import { JSDOM } from 'jsdom';
+import {streakCounter} from '../src/index';
+import {formattedDate  } from '../src/lib';
+
 describe("Testing the tester 🎙", () => {
   it("should pass", () => {
     expect(true).toBe(true);
   });
+})
+
+describe("Streak Counter", () => {
+  let mockLocalStorage: Storage;
+  beforeEach(() => {
+    const mockJSDom = new JSDOM("", { url: "https://localhost" });
+
+    mockLocalStorage = mockJSDom.window.localStorage;
+  });
+
+  it("should return a streak object with currentCount, startDate, lastLoginDate", () => {
+    const date = new Date();
+    const streak = streakCounter(mockLocalStorage, date);
+
+    expect(streak.hasOwnProperty("currentCount")).toBe(true);
+    expect(streak.hasOwnProperty("startDate")).toBe(true);
+    expect(streak.hasOwnProperty("lastLoginDate")).toBe(true);
+
+  })
+
+  it("should return a streak starting at 1 and keep track of lastLoginDate", () => {
+    const date = new Date();
+    const streak = streakCounter(mockLocalStorage, date);
+
+    const dateFormatted = formattedDate(date);
+
+    expect(streak.currentCount).toBe(1);
+    expect(streak.lastLoginDate).toBe(dateFormatted);
+  })
 })
