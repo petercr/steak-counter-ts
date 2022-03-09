bdc40255aed4d404f8bf613b0208389d3550b1ab
Peter <petecruckshank@gmail.com>
2022-03-09 17:39:35 -0500

    🎨 changed out variable name

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
-import { formattedDate } from '../src/lib';
+import { formattedDate } from "../src/lib";
 
 // Used when storing in localStorage
 const KEY = "streak";
 
-interface Streak  {
+interface Streak {
   currentCount: number;
   startDate: string;
   lastLoginDate: string;
 }
 
-
-
-export function streakCounter(_localStorage: Storage, date: Date): Streak {
+export function streakCounter(storage: Storage, date: Date): Streak {
   const streak = {
     currentCount: 1,
     startDate: formattedDate(date),
-    lastLoginDate:formattedDate(date)
-  }
+    lastLoginDate: formattedDate(date),
+  };
   // store in localStorage
-  _localStorage.setItem(KEY, JSON.stringify(streak));
+  storage.setItem(KEY, JSON.stringify(streak));
 
   return streak;
 }
