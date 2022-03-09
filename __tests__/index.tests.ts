8e5dbae5b1b478c9ba362e2d4f2231bf69eeeed8
Peter <petecruckshank@gmail.com>
2022-03-09 17:55:34 -0500

    ✅ new test to check return of existing streak {}

--- __tests__/index.tests.ts
+++ __tests__/index.tests.ts
@@ -1,12 +1,12 @@
-import { JSDOM } from 'jsdom';
-import {streakCounter} from '../src/index';
-import {formattedDate  } from '../src/lib';
+import { JSDOM } from "jsdom";
+import { streakCounter } from "../src/index";
+import { formattedDate } from "../src/lib";
 
 describe("Testing the tester 🎙", () => {
   it("should pass", () => {
     expect(true).toBe(true);
   });
-})
+});
 
 describe("Streak Counter", () => {
   let mockLocalStorage: Storage;
@@ -16,6 +16,10 @@ describe("Streak Counter", () => {
     mockLocalStorage = mockJSDom.window.localStorage;
   });
 
+  afterEach(() => {
+    mockLocalStorage.clear();
+  });
+
   it("should return a streak object with currentCount, startDate, lastLoginDate", () => {
     const date = new Date();
     const streak = streakCounter(mockLocalStorage, date);
@@ -23,8 +27,7 @@ describe("Streak Counter", () => {
     expect(streak.hasOwnProperty("currentCount")).toBe(true);
     expect(streak.hasOwnProperty("startDate")).toBe(true);
     expect(streak.hasOwnProperty("lastLoginDate")).toBe(true);
-
-  })
+  });
 
   it("should return a streak starting at 1 and keep track of lastLoginDate", () => {
     const date = new Date();
@@ -34,7 +37,7 @@ describe("Streak Counter", () => {
 
     expect(streak.currentCount).toBe(1);
     expect(streak.lastLoginDate).toBe(dateFormatted);
-  })
+  });
 
   it("should store the streak in localStorage", () => {
     const date = new Date();
@@ -44,5 +47,36 @@ describe("Streak Counter", () => {
     const streakAsString = mockLocalStorage.getItem(key);
 
     expect(streakAsString).not.toBeNull();
-  })
-})
+  });
+
+  describe("with a pre-populated streak", () => {
+    let mockLocalStorage: Storage;
+    beforeEach(() => {
+      const mockJSDom = new JSDOM("", { url: "https://localhost" });
+
+      mockLocalStorage = mockJSDom.window.localStorage;
+
+      // Use date in past so it doesn't
+      // conflict if test uses date test runs.
+      const date = new Date("2021-12-12");
+
+      const streak = {
+        currentCount: 1,
+        startDate: formattedDate(date),
+        lastLoginDate: formattedDate(date),
+      };
+
+      mockLocalStorage.setItem("streak", JSON.stringify(streak));
+    });
+    afterEach(() => {
+      mockLocalStorage.clear();
+    });
+    it("should return the streak from localStorage", () => {
+      const date = new Date();
+      const streak = streakCounter(mockLocalStorage, date);
+
+      // Should match the dates used to set up the tests
+      expect(streak.startDate).toBe("12/11/2021");
+    });
+  });
+});
