7c3757bea8f6d2153f712d0c554407332a666cba
Peter <petecruckshank@gmail.com>
2022-03-14 17:12:55 -0400

    ✨ add func to plus or reset localStorage streak

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,50 @@ function assertStreakExists(
   return streakInLocalStorage !== null && streakInLocalStorage !== "";
 }
 
+function shouldIncrementOrRestStreakCount(
+  currentDate: Date,
+  lastLoginDate: string
+): "increment" | undefined {
+  // Get the current date as a string, minus the date from lastLoginDate
+  // split the results of lastLoginDate and take just the date
+  const difference =
+    currentDate.getDate() - parseInt(lastLoginDate.split("/")[1]);
+
+  if (difference === 1) {
+    return "increment";
+  }
+
+  // the streak has ended
+  return undefined;
+}
+
 export function streakCounter(storage: Storage, date: Date): Streak {
   const streakInLocalStorage = storage.getItem(KEY);
-  const doesStreakExist =
-    streakInLocalStorage !== null && streakInLocalStorage !== "";
+  if (streakInLocalStorage) {
+    try {
+      const streak = JSON.parse(streakInLocalStorage);
+      const state = shouldIncrementOrRestStreakCount(
+        date,
+        streak.lastLoginDate
+      );
+      const SHOULD_INCREMENT = state === "increment";
+
+      if (SHOULD_INCREMENT) {
+        const updatedStreak: Streak = {
+          ...streak,
+          currentCount: streak.currentCount + 1,
+          lastLoginDate: formattedDate(date),
+        };
+        // store in local storage
+        storage.setItem(KEY, JSON.stringify(updatedStreak));
+
+        return updatedStreak;
+      }
+      return streak;
+    } catch (error) {
+      console.error("Failed to parse streak from localStorage", error);
+    }
+  }
 
   if (assertStreakExists(streakInLocalStorage)) {
     try {
