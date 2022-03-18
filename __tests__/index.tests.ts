003904c5328cc742bb19a0641871f542d8380569
Peter <petecruckshank@gmail.com>
2022-03-18 18:09:15 -0400

    ✅ tests reset, saving to LS, and same day login

--- __tests__/index.tests.ts
+++ __tests__/index.tests.ts
@@ -72,7 +72,7 @@ describe("Streak Counter", () => {
       mockLocalStorage.clear();
     });
     it("should return the streak from localStorage", () => {
-      const date = new Date();
+      const date = new Date("12/11/2021");
       const streak = streakCounter(mockLocalStorage, date);
 
       // Should match the dates used to set up the tests
@@ -102,6 +102,46 @@ describe("Streak Counter", () => {
       const streak = JSON.parse(streakAsString || "");
 
       expect(streak.currentCount).toBe(2);
+    });
+    it("should reset if not consecutive days", () => {
+      const date = new Date("12/12/2021");
+      const streak = streakCounter(mockLocalStorage, date);
+
+      expect(streak.currentCount).toBe(2);
+
+      // skip a day and break the streak
+      const dateUpdated = new Date("12/15/2021");
+      const streakUpdated = streakCounter(mockLocalStorage, dateUpdated);
+
+      expect(streakUpdated.currentCount).toBe(1);
+    });
+    it("should save the reset streak to localStorage", () => {
+      const key = "streak";
+      const date = new Date("12/13/2021");
+      // Call it once so it updates the streak
+      streakCounter(mockLocalStorage, date);
+
+      // Skip a day and break the streak
+      const dateUpdated = new Date("12/15/2021");
+      const streakUpdated = streakCounter(mockLocalStorage, dateUpdated);
+
+      const streakAsString = mockLocalStorage.getItem(key);
+      // Normally you should wrap in try/catch in case the JSON is bad
+      // but since we authored it, we can skip here
+      const streak = JSON.parse(streakAsString || "");
+
+      expect(streak.currentCount).toBe(1);
+    });
+    it("should not reset the streak for same-day login", () => {
+      const date = new Date("12/13/2021");
+      // Call it once so it updates the streak
+      streakCounter(mockLocalStorage, date);
+
+      // Simulate same-day login
+      const dateUpdated = new Date("12/13/2021");
+      const streakUpdated = streakCounter(mockLocalStorage, dateUpdated);
+
+      expect(streakUpdated.currentCount).toBe(1);
     });
   });
 });
